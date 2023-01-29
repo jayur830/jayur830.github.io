5db563529171448e11fc782f5b054494aedaea2c
jayur830 <jayur830@naver.com>
2023-01-30 02:04:59 +0900

    .

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('NODE_SERVER_PORT');
+  const port = configService.get('PORT');
   await app.listen(port);
   console.log(`Listen port ${port}.`);
 }

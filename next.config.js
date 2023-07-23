fcfad107045a6b106cb6860b1e6113c719c454a6
jayur830 <jayur830@naver.com>
2023-07-23 20:47:20 +0900

    ci: docs 디렉토리 삭제

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'docs',
+  distDir: 'dist',
   images: {
     unoptimized: true,
   },

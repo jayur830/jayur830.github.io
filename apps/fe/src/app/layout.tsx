0d803646f589e9e38934b68ece25402c97b8160d
jayur830 <jayur830@naver.com>
2023-08-01 20:48:14 +0900

    feat(fe): gtm 다시 세팅

--- apps/fe/src/app/layout.tsx
+++ apps/fe/src/app/layout.tsx
@@ -1,9 +1,6 @@
 import { PropsWithChildren } from 'react';
 
-import TagManager from 'react-gtm-module';
-
 export default function RootLayout({ children }: PropsWithChildren) {
-  TagManager.initialize({ gtmId: process.env.GTM_ID });
   return (
     <html>
       <body>{children}</body>

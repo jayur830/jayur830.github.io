555a49e059e98e1b233ce78b3f01ac8d228f967e
jayur830 <jayur830@users.noreply.github.com>
2023-11-28 12:44:11 +0000

    deploy: d37c80629edabc26cb48f7818dbf97349df4c5bb

---
+++ _next/static/Gv5fEa3RoIyRPOhz0oQ3R/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-68ade602aead59a5.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

23dead45abb1ccd6401a3224187984de705a9d55
jayur830 <jayur830@naver.com>
2024-06-19 23:12:35 +0900

    fix: 다크모드일 때 헤더 `backgroudColor`가 여전히 `white`로 남아있음

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -17,7 +17,7 @@ export default function Header({ navigations, extra }: HeaderProps) {
 
   return (
     <>
-      <Grid position="fixed" display="flex" justifyContent="space-between" alignItems="center" width="100%" height={60} zIndex={99} sx={{ backgroundColor: 'common.white' }}>
+      <Grid position="fixed" display="flex" justifyContent="space-between" alignItems="center" width="100%" height={60} zIndex={99} sx={(theme) => ({ backgroundColor: theme.palette.mode })}>
         {process.env.NODE_ENV === 'development' ? (
           <IconButton size="large" onClick={() => setOpen(true)}>
             <DensityMedium />

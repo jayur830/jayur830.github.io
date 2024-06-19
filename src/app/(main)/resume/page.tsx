7be912eccbae909099c4c59789f40d1e73d5536b
jayur830 <jayur830@naver.com>
2024-06-20 00:13:45 +0900

    revert: ssr → csr 원복
    - 생각해보니 ssr 해버리면 static 페이지라 빌드 타임에 컨텐츠가 영원히 고정됨
    - DB가 수정되어도 페이지가 안바뀔 수 있음

--- src/app/(main)/resume/page.tsx
+++ src/app/(main)/resume/page.tsx
@@ -1,41 +1,38 @@
+'use client';
+
+import { useQuery } from '@apollo/client';
 import { GitHub } from '@mui/icons-material';
 import { Grid, IconButton, Typography } from '@mui/material';
+import { useEffect, useMemo } from 'react';
 
 import ResumeCard from '@/components/ResumeCard';
-import client from '@/graphql/apollo';
+import { useOnChangeLoading } from '@/contexts/LoadingProvider';
 import { ResumeQuery, ResumeQueryVariables } from '@/graphql/graphql';
 import RESUME_QUERY from '@/graphql/queries/getResume.gql';
 
-export default async function Page() {
-  // const onChangeLoading = useOnChangeLoading();
+export default function Page() {
+  const onChangeLoading = useOnChangeLoading();
 
-  // const { data, loading } = useQuery<ResumeQuery, ResumeQueryVariables>(GET_RESUME_QUERY, {
-  //   variables: {
-  //     userId: process.env.NEXT_PUBLIC_UID,
-  //   },
-  // });
-
-  // const resumeData = useMemo(() => {
-  //   if (!data) {
-  //     return null;
-  //   }
-
-  //   return data.resume;
-  // }, [data]);
-
-  // useEffect(() => {
-  //   onChangeLoading(loading);
-  //   return () => {
-  //     onChangeLoading(false);
-  //   };
-  // }, [onChangeLoading, loading]);
-  const { data } = await client.query<ResumeQuery, ResumeQueryVariables>({
-    query: RESUME_QUERY,
+  const { data, loading } = useQuery<ResumeQuery, ResumeQueryVariables>(RESUME_QUERY, {
     variables: {
       userId: process.env.NEXT_PUBLIC_UID,
     },
   });
-  const resumeData = data.resume;
+
+  const resumeData = useMemo(() => {
+    if (!data) {
+      return null;
+    }
+
+    return data.resume;
+  }, [data]);
+
+  useEffect(() => {
+    onChangeLoading(loading);
+    return () => {
+      onChangeLoading(false);
+    };
+  }, [onChangeLoading, loading]);
 
   return (
     <>
@@ -47,16 +44,11 @@ export default async function Page() {
           <IconButton href={resumeData?.github} target="_blank">
             <GitHub
               color="action"
-              // sx={(theme) => ({
-              //   width: { xs: 25, md: 40 },
-              //   height: { xs: 25, md: 40 },
-              //   fill: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
-              // })}
-              sx={{
+              sx={(theme) => ({
                 width: { xs: 25, md: 40 },
                 height: { xs: 25, md: 40 },
-                // fill: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
-              }}
+                fill: theme.palette.mode === 'dark' ? theme.palette.common.white : theme.palette.common.black,
+              })}
             />
           </IconButton>
         )}

0896e2f842adece4f77ed2007ee808db40320878
jayur830 <jayur830@naver.com>
2025-08-22 17:53:09 +0900

    chore: next.js 15 세팅, eslint 설정

--- src/types/common.ts
+++
@@ -1,3 +0,0 @@
-import type { PropsWithChildren, ReactNode } from 'react';
-
-export type PropsWithSegment<T extends string> = PropsWithChildren<{ [key in T]: ReactNode }>;
--- src/types/navigation.ts
+++
@@ -1,4 +0,0 @@
-export interface NavigationItem {
-  label: string;
-  path: string;
-}

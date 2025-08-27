db4e704d66b4c5e51684310c4d9f5cf7f417ae30
jayur830 <jayur830@naver.com>
2025-08-28 00:12:13 +0900

    fix: 다크모드 스위치 Hydration 에러 수정

--- src/components/service/DarkModeSwitch.tsx
+++ src/components/service/DarkModeSwitch.tsx
@@ -2,15 +2,36 @@
 
 import { MoonIcon, SunIcon } from 'lucide-react';
 import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 import { Switch } from '@/components/ui/switch';
 
 export default function DarkModeSwitch() {
   const { theme, setTheme, resolvedTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
 
   const handleDarkMode = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
+
+  if (!mounted) {
+    return (
+      <Switch
+        checked={false}
+        className="w-11 h-6"
+        disabled
+        thumbProps={{ asChild: true }}
+      >
+        <div className="flex justify-center items-center size-[22px] bg-white rounded-full">
+          <SunIcon className="size-3" />
+        </div>
+      </Switch>
+    );
+  }
 
   const isDarkMode = resolvedTheme === 'dark';
 

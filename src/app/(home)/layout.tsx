d8b7391833ba6ccbcb95f437d84d6cf4177250cc
jayur830 <jayur830@naver.com>
2025-09-11 15:55:44 +0900

    fix: `info` 영역 추가, `about` 영역 비워둠

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,8 +1,9 @@
 import type { PropsWithChildren, ReactNode } from 'react';
 
-export default function Layout({ children, about, skills, projects }: PropsWithChildren<Record<'about' | 'skills' | 'projects', ReactNode>>) {
+export default function Layout({ children, info, about, skills, projects }: PropsWithChildren<Record<'info' | 'about' | 'skills' | 'projects', ReactNode>>) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
+      {info}
       {about}
       {skills}
       {projects}
